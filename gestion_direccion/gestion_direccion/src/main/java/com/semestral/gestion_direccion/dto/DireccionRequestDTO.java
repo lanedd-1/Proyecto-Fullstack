6637a9d95221151d469af469eb4c13281236f2b0
lanedd-1 <j.recabarren@duocuc.cl>
2026-05-04 21:14:56 -0400

    DTO Direccion
    agrega cambios DTO

---
+++ gestion_direccion/gestion_direccion/src/main/java/com/semestral/gestion_direccion/dto/DireccionRequestDTO.java
@@ -0,0 +1,24 @@
+package com.semestral.gestion_direccion.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DireccionRequestDTO {
+    @NotBlank
+    private String calle;
+    @NotBlank
+    private String numero;
+    @NotNull
+    @Positive
+    private Long id_comuna;
+    @NotNull
+    @Positive
+    private Long id_estado;
+}
---
+++ gestion_direccion/gestion_direccion/src/main/java/com/semestral/gestion_direccion/dto/DireccionResponseDTO.java
@@ -0,0 +1,16 @@
+package com.semestral.gestion_direccion.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DireccionResponseDTO {
+    private Long id_direccion;
+    private String calle;
+    private String numero;
+    private Long id_comuna;
+    private Long id_Region;
+}

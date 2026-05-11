7d93912f7721e556a706fe0591cb11822974b29f
renata-valdesr <babubub4a@gmail.com>
2026-05-11 10:06:04 -0400

    gestionestadov1
    Agregue la gestion estado BASE solo con datos en properties y carpetas basicas vacias

---
+++ gestionestado/src/main/java/com/joyeria/gestionestado/GestionestadoApplication.java
@@ -0,0 +1,13 @@
+package com.joyeria.gestionestado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionestadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionestadoApplication.class, args);
+	}
+
+}
---
+++ gestionestado/src/test/java/com/joyeria/gestionestado/GestionestadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyeria.gestionestado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionestadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

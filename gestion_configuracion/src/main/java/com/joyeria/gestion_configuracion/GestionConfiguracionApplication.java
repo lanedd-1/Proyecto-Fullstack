10788dcb158fbc222ca457e2b2ac4e1f76ff33c8
renata-valdesr <babubub4a@gmail.com>
2026-05-16 22:07:11 -0400

    Se agrega ms gestion Configuracion
    agregue la carpeta del microservicio de gestion configuracion vacia

---
+++ gestion_configuracion/src/main/java/com/joyeria/gestion_configuracion/GestionConfiguracionApplication.java
@@ -0,0 +1,13 @@
+package com.joyeria.gestion_configuracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionConfiguracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionConfiguracionApplication.class, args);
+	}
+
+}
---
+++ gestion_configuracion/src/test/java/com/joyeria/gestion_configuracion/GestionConfiguracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyeria.gestion_configuracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionConfiguracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

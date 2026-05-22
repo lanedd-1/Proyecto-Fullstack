8b309c5804d363d576737184209dd41dcd42b3f2
lanedd-1 <j.recabarren@duocuc.cl>
2026-05-22 16:47:24 -0400

    faltaban cosas

---
+++ gestion_envio/src/test/java/com/joyeria/gestion_envio/GestionEnvioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyeria.gestion_envio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEnvioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

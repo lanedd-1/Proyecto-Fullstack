faf360aba664b72bd5e5d631f305f389f6cda3c3
Matias Olavarria <elshield.ever.06@gmail.com>
2026-06-16 14:23:15 -0400

    se agrega la documentacion
    se documenta el CRUD de inventario usando @Tag, @Operation, @ApiResponses.

--- inventarioSemestral/inventario/src/main/java/com/semestral/inventario/repository/EstanteRepository.java
+++ inventarioSemestral/inventario/src/main/java/com/semestral/inventario/repository/EstanteRepository.java
@@ -1,11 +1,11 @@
 package com.semestral.inventario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.semestral.inventario.model.Estante;
 
-@Repository
+
 public interface EstanteRepository extends JpaRepository<Estante, Long> {
     
 }

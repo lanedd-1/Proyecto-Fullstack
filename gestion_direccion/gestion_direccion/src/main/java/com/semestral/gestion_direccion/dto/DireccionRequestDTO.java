02a5bb47d9ec2453832273bf16351e795ec1f3ea
lanedd-1 <j.recabarren@duocuc.cl>
2026-06-17 10:02:14 -0400

    Documentacion de DTO Direccion

--- gestion_direccion/gestion_direccion/src/main/java/com/semestral/gestion_direccion/dto/DireccionRequestDTO.java
+++ gestion_direccion/gestion_direccion/src/main/java/com/semestral/gestion_direccion/dto/DireccionRequestDTO.java
@@ -1,5 +1,6 @@
 package com.semestral.gestion_direccion.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -12,17 +13,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DireccionRequestDTO {
     @NotBlank(message = "La calle no puede estar vacia")
+    @Schema(description = "Nombre de la calle", example = "Avenida Siempre Viva")
     private String calle;
     @NotBlank(message = "El numero no puede estar vacio")
+    @Schema(description = "Numero de la Calle registrada", example = "123")
     private String numero;
     @NotNull(message = "El id no puede ser nula")
     @Positive(message = "el id debe ser mayor a 0")
+    @Schema(description = "ID de la comuna asociada", example = "1")
     private Long idComuna;
     @NotNull(message = "El id no puede ser nula")
     @Positive(message = "el id debe ser mayor a 0")
+    @Schema(description = "ID del usuario asociada", example = "1")
     private Long idUsuario;
     @NotNull(message = "El id no puede ser nula")
     @Positive(message = "el id debe ser mayor a 0")
+    @Schema(description = "ID del estado asociado", example = "1")
     private Long idEstado;
 
 }

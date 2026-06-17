02d72ed4ae902a505e16480edafa714ca99d0042
lanedd-1 <j.recabarren@duocuc.cl>
2026-06-17 09:46:19 -0400

    Documentacion de los modelos Direccion

--- gestion_direccion/gestion_direccion/src/main/java/com/semestral/gestion_direccion/model/Comuna.java
+++ gestion_direccion/gestion_direccion/src/main/java/com/semestral/gestion_direccion/model/Comuna.java
@@ -1,5 +1,6 @@
 package com.semestral.gestion_direccion.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,14 +18,22 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Entidad que representa a una comuna dentro del sistema de gestión")
 public class Comuna {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(
+        description = "Identificador único autogenerado por la base de datos", 
+        example = "1", 
+        accessMode = Schema.AccessMode.READ_ONLY
+    )
     private Long idComuna;
+    @Schema(description = "Nombre de la comuna registrada", example = "Metropolitana")
     @Column(name = "nombreComuna")
     private String nombreC;
     @ManyToOne
     @JoinColumn(name = "idRegion")
+    @Schema(description = "Objeto completo de la region asignado con sus respectivos detalles")
     private Region region;
     
 }
--- gestion_direccion/gestion_direccion/src/main/java/com/semestral/gestion_direccion/model/Region.java
+++ gestion_direccion/gestion_direccion/src/main/java/com/semestral/gestion_direccion/model/Region.java
@@ -1,5 +1,6 @@
 package com.semestral.gestion_direccion.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,7 +19,14 @@ import lombok.NoArgsConstructor;
 public class Region {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(
+        description = "Identificador único autogenerado por la base de datos", 
+        example = "1", 
+        accessMode = Schema.AccessMode.READ_ONLY
+    )
     private Long idRegion;
+
     @Column(name = "nombreRegion",nullable = false)
+    @Schema(description = "Nombre de la region registrada", example = "Metropolitana")
     private String nombreRegion;
 }

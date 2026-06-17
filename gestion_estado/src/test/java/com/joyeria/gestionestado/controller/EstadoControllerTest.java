3c9529a746a6310461c22d33ca127c73ca454dee
renata-valdesr <babubub4a@gmail.com>
2026-06-17 19:14:52 -0400

    5% del controller test ms estado
    no pregunten ok algo es algo

--- gestion_estado/src/test/java/com/joyeria/gestionestado/controller/EstadoControllerTest.java
+++ gestion_estado/src/test/java/com/joyeria/gestionestado/controller/EstadoControllerTest.java
@@ -1,5 +1,29 @@
 package com.joyeria.gestionestado.controller;
 
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import com.joyeria.gestionestado.Service.EstadoService;
+
+
+@WebMvcTest(EstadoController.class)
+@DisplayName("Tests del EstadoController con MockMvc")
 public class EstadoControllerTest {
+
+    //Simula peticiones HTTP sin levantar un servidor real
+    @Autowired
+    private MockMvc mockMvc;
+
+    //Mock del service inyectado en el controller
+    @MockitoBean
+    private EstadoService estadoService;
+
+    //Convierte objetos Java a JSON para los endpoints
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
 
 }

dcd7147bedbac5364ba5670e0093e82edaf0e891
renata-valdesr <babubub4a@gmail.com>
2026-06-17 21:45:44 -0400

    ... Import ms estado controller test
    Import en el controller test del ms estado pq en mi compu no me salen asi que los hice a mano

--- gestion_estado/src/test/java/com/joyeria/gestionestado/controller/EstadoControllerTest.java
+++ gestion_estado/src/test/java/com/joyeria/gestionestado/controller/EstadoControllerTest.java
@@ -2,14 +2,27 @@ package com.joyeria.gestionestado.controller;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 import com.joyeria.gestionestado.Service.EstadoService;
+import com.joyeria.gestionestado.dto.EstadoRequestDTO;
+import com.joyeria.gestionestado.dto.EstadoResponseDTO;
 
 
 @WebMvcTest(EstadoController.class)

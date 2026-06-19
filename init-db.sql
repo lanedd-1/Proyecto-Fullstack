429a16ddba407a4b3bcc1238f83c0e41fde11d7f
renata-valdesr <babubub4a@gmail.com>
2026-06-18 22:11:37 -0400

    adelanto init-db.sql
    esta comentado por QUE NO ESTAN EL ORDEN QUE DEEBERIAN IR OFICIAL

---
+++ init-db.sql
@@ -0,0 +1,48 @@
+--ESTO DEBERIA IR EN ORDEN BTW
+
+-- MS_CONFIGURACION
+--CREATE DATABASE IF NOT EXISTS db_configuracion
+ --CHARACTER SET utf8mb4
+ --COLLATE utf8mb4_unicode_ci;
+
+-- MS_DIRECCION
+--CREATE DATABASE IF NOT EXISTS db_direccion
+ --CHARACTER SET utf8mb4
+ --COLLATE utf8mb4_unicode_ci;
+
+-- MS_ENVIO
+--CREATE DATABASE IF NOT EXISTS db_envio
+ --CHARACTER SET utf8mb4
+ --COLLATE utf8mb4_unicode_ci;
+
+-- MS_USUARIOS
+--CREATE DATABASE IF NOT EXISTS db_usuarios
+ --CHARACTER SET utf8mb4
+ --COLLATE utf8mb4_unicode_ci;
+
+-- MS_SEMESTRAL --> INVENTARIO
+--CREATE DATABASE IF NOT EXISTS db_inventario
+ --CHARACTER SET utf8mb4
+ --COLLATE utf8mb4_unicode_ci;
+
+-- MS_PRODUCTOS
+--CREATE DATABASE IF NOT EXISTS db_productos
+ --CHARACTER SET utf8mb4
+ --COLLATE utf8mb4_unicode_ci;
+
+-- MS_VENTAS
+--CREATE DATABASE IF NOT EXISTS db_ventas
+ --CHARACTER SET utf8mb4
+ --COLLATE utf8mb4_unicode_ci;
+
+ -- Otorgar todos los permisos al usuario root
+--GRANT ALL PRIVILEGES ON db_configuracion.* TO 'root'@'%';
+--GRANT ALL PRIVILEGES ON db_direccion.* TO 'root'@'%';
+--GRANT ALL PRIVILEGES ON db_envio.* TO 'root'@'%';
+--GRANT ALL PRIVILEGES ON db_usuarios.* TO 'root'@'%';
+--GRANT ALL PRIVILEGES ON db_inventario.* TO 'root'@'%';
+--GRANT ALL PRIVILEGES ON db_productos.* TO 'root'@'%';
+--GRANT ALL PRIVILEGES ON db_ventas.* TO 'root'@'%';
+
+-- Permisos inmediatamente
+--FLUSH PRIVILEGES;

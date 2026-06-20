dc5814fc255aabc92e421615ddc52092ca05430e
lanedd-1 <j.recabarren@duocuc.cl>
2026-06-19 20:23:46 -0400

    configuracion de eureka
    ms de usuarios, direccion y envio

--- gestion_usuarios/src/main/java/com/semestral/gestion_usuarios/GestionUsuariosApplication.java
+++ gestion_usuarios/src/main/java/com/semestral/gestion_usuarios/GestionUsuariosApplication.java
@@ -2,10 +2,12 @@ package com.semestral.gestion_usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class GestionUsuariosApplication {
 
 	public static void main(String[] args) {

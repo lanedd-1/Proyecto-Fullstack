986be491e97844b2f425b93a0b4bc04014c4915b
renata-valdesr <babubub4a@gmail.com>
2026-06-19 23:43:29 -0400

    docker cositas completado

--- init-db.sql
+++ init-db.sql
@@ -1,47 +1,47 @@
 
 -- MS_CONFIGURACION
---CREATE DATABASE IF NOT EXISTS db_configuracion
- --CHARACTER SET utf8mb4
- --COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS db_configuracion
+ CHARACTER SET utf8mb4
+ COLLATE utf8mb4_unicode_ci;
 
 -- MS_USUARIOS
---CREATE DATABASE IF NOT EXISTS db_usuarios
- --CHARACTER SET utf8mb4
- --COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS db_usuarios
+ CHARACTER SET utf8mb4
+ COLLATE utf8mb4_unicode_ci;
 
 -- MS_DIRECCION
---CREATE DATABASE IF NOT EXISTS db_direccion
- --CHARACTER SET utf8mb4
- --COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS db_direccion
+ CHARACTER SET utf8mb4
+ COLLATE utf8mb4_unicode_ci;
 
 -- MS - INVENTARIO
---CREATE DATABASE IF NOT EXISTS db_inventario
- --CHARACTER SET utf8mb4
- --COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS db_inventario
+ CHARACTER SET utf8mb4
+ COLLATE utf8mb4_unicode_ci;
 
 -- MS_PRODUCTOS
---CREATE DATABASE IF NOT EXISTS db_productos
- --CHARACTER SET utf8mb4
- --COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS db_productos
+ CHARACTER SET utf8mb4
+ COLLATE utf8mb4_unicode_ci;
 
 -- MS_ENVIO
---CREATE DATABASE IF NOT EXISTS db_envio
- --CHARACTER SET utf8mb4
- --COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS db_envio
+ CHARACTER SET utf8mb4
+ COLLATE utf8mb4_unicode_ci;
 
 -- MS_VENTAS
---CREATE DATABASE IF NOT EXISTS db_ventas
- --CHARACTER SET utf8mb4
- --COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS db_ventas
+ CHARACTER SET utf8mb4
+ COLLATE utf8mb4_unicode_ci;
 
  -- Otorgar todos los permisos al usuario root
---GRANT ALL PRIVILEGES ON db_configuracion.* TO 'root'@'%';
---GRANT ALL PRIVILEGES ON db_direccion.* TO 'root'@'%';
---GRANT ALL PRIVILEGES ON db_envio.* TO 'root'@'%';
---GRANT ALL PRIVILEGES ON db_usuarios.* TO 'root'@'%';
---GRANT ALL PRIVILEGES ON db_inventario.* TO 'root'@'%';
---GRANT ALL PRIVILEGES ON db_productos.* TO 'root'@'%';
---GRANT ALL PRIVILEGES ON db_ventas.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON db_configuracion.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON db_usuarios.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON db_direccion.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON db_inventario.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON db_productos.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON db_envio.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON db_ventas.* TO 'root'@'%';
 
 -- Permisos inmediatamente
---FLUSH PRIVILEGES;
+FLUSH PRIVILEGES;

bfaad0fea650e70075ec67f819d23c360da55211
renata-valdesr <babubub4a@gmail.com>
2026-06-19 23:10:56 -0400

    se agrega productos a eureka

--- init-db.sql
+++ init-db.sql
@@ -1,17 +1,6 @@
---ESTO DEBERIA IR EN ORDEN BTW
 
 -- MS_CONFIGURACION
 --CREATE DATABASE IF NOT EXISTS db_configuracion
- --CHARACTER SET utf8mb4
- --COLLATE utf8mb4_unicode_ci;
-
--- MS_DIRECCION
---CREATE DATABASE IF NOT EXISTS db_direccion
- --CHARACTER SET utf8mb4
- --COLLATE utf8mb4_unicode_ci;
-
--- MS_ENVIO
---CREATE DATABASE IF NOT EXISTS db_envio
  --CHARACTER SET utf8mb4
  --COLLATE utf8mb4_unicode_ci;
 
@@ -20,13 +9,23 @@
  --CHARACTER SET utf8mb4
  --COLLATE utf8mb4_unicode_ci;
 
--- MS_SEMESTRAL --> INVENTARIO
+-- MS_DIRECCION
+--CREATE DATABASE IF NOT EXISTS db_direccion
+ --CHARACTER SET utf8mb4
+ --COLLATE utf8mb4_unicode_ci;
+
+-- MS - INVENTARIO
 --CREATE DATABASE IF NOT EXISTS db_inventario
  --CHARACTER SET utf8mb4
  --COLLATE utf8mb4_unicode_ci;
 
 -- MS_PRODUCTOS
 --CREATE DATABASE IF NOT EXISTS db_productos
+ --CHARACTER SET utf8mb4
+ --COLLATE utf8mb4_unicode_ci;
+
+-- MS_ENVIO
+--CREATE DATABASE IF NOT EXISTS db_envio
  --CHARACTER SET utf8mb4
  --COLLATE utf8mb4_unicode_ci;
 

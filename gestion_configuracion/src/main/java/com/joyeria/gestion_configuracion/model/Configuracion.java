d09a6c399f17cc7eea25b6e629d22da9a4843989
renata-valdesr <babubub4a@gmail.com>
2026-06-19 20:08:35 -0400

    cambios

--- gestion_configuracion/src/main/java/com/joyeria/gestion_configuracion/model/Configuracion.java
+++ gestion_configuracion/src/main/java/com/joyeria/gestion_configuracion/model/Configuracion.java
@@ -1,7 +1,10 @@
 package com.joyeria.gestion_configuracion.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -13,18 +16,40 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "Configuracion")
+@Schema(description = "Entidad que representa la configuraccion de reglas de contraseña")
 public class Configuracion {
+
     @Id
     @Column(name = "id_configuracion")
+    @Schema(
+        description = "Identificador único generaod automáticamente por la BD",
+        example = "1",
+        accessMode = Schema.AccessMode.READ_ONLY
+    )
     private Long idConfiguracion;
 
+
     @Column(name = "longitud_minima", nullable = false)
+    @Schema(
+        description = "Longitud mínima permitida para la contraseña",
+        example = "8"
+    )
     private Integer longitudMinima;
 
+
     @Column(name = "longitud_maxima", nullable = false)
+    @Schema(
+        description = "Longitud máxima permitida para la contraseña",
+        example = "20"
+    )
     private Integer longitudMaxima;
 
+
     @Column(name = "requiere_mayuscula", nullable = false)
+    @Schema(
+        description = "Indica si la contraseña debe contener al menos una letra mayúscula",
+        example = "true"
+    )
     private Boolean requiereMayuscula;
 
     @Column(name = "requiere_minuscula", nullable = false)

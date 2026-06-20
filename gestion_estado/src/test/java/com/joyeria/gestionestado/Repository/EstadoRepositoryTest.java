426cfb5458aae890e1e46a11d319222b60cacee2
renata-valdesr <babubub4a@gmail.com>
2026-06-19 22:54:19 -0400

    BUGS DE NUEVO CAMBIO D NOMBRES

--- gestion_estado/src/test/java/com/joyeria/gestionestado/Repository/EstadoRepositoryTest.java
+++ gestion_estado/src/test/java/com/joyeria/gestionestado/Repository/EstadoRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.joyeria.gestionestado.Repository;
+package com.joyeria.gestionestado.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;

b543257c53a5a64b3eb18a34900ffc51ca542606
renata-valdesr <babubub4a@gmail.com>
2026-06-19 21:04:44 -0400

    bugs 3 ...

--- gestion_configuracion/src/main/java/com/joyeria/gestion_configuracion/model/Configuracion.java
+++ gestion_configuracion/src/main/java/com/joyeria/gestion_configuracion/model/Configuracion.java
@@ -3,8 +3,7 @@ package com.joyeria.gestion_configuracion.model;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;

43c1324d60cd7dab1069479677a57ff0546e81ab
lanedd-1 <j.recabarren@duocuc.cl>
2026-06-20 23:59:07 -0400

    bugs fixed
    dependencias e imports

--- productos-microservicio-main/src/main/java/com/semestral/productos/controller/CategoriaController.java
+++ productos-microservicio-main/src/main/java/com/semestral/productos/controller/CategoriaController.java
@@ -3,12 +3,13 @@ package com.semestral.productos.controller;
 import java.util.List;
 import java.util.Optional;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- productos-microservicio-main/src/test/java/com/semestral/productos/controller/CategoriaControllerTest.java
+++ productos-microservicio-main/src/test/java/com/semestral/productos/controller/CategoriaControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -84,4 +85,3 @@ class CategoriaControllerTest {
             .andExpect(jsonPath("$.nombreCat").value("Aros"));
     }
 }
-

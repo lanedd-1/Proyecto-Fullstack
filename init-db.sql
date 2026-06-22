832cf8b5c65759d34bc1a7353915824d984a1a46
renata-valdesr <babubub4a@gmail.com>
2026-06-21 21:10:53 -0400

    limpieza de comentarios y bugs

--- init-db.sql
+++ init-db.sql
@@ -34,7 +34,6 @@ CREATE DATABASE IF NOT EXISTS db_ventas
  CHARACTER SET utf8mb4
  COLLATE utf8mb4_unicode_ci;
 
- -- Otorgar todos los permisos al usuario root
 GRANT ALL PRIVILEGES ON db_usuarios.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON db_configuracion.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON db_direccion.* TO 'root'@'%';
@@ -43,5 +42,4 @@ GRANT ALL PRIVILEGES ON db_productos.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON db_envio.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON db_ventas.* TO 'root'@'%';
 
--- Permisos inmediatamente
 FLUSH PRIVILEGES;
